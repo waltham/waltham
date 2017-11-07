b4d5d76ca6958b856756815f05fe9cef6a55bd88
Pekka Paalanen <pekka.paalanen@collabora.co.uk>
2017-11-07 16:16:03 +0200

    Update docs from rev v0.0.16-163-gc95707421b5ce9973ea3e34fc7e20d151c4c6d5c.
    Doxygen update.

---
+++ menudata.js
@@ -0,0 +1,17 @@
+var menudata={children:[
+{text:"Main Page",url:"index.html"},
+{text:"Related Pages",url:"pages.html"},
+{text:"Modules",url:"modules.html"},
+{text:"Data Structures",url:"annotated.html",children:[
+{text:"Data Structures",url:"annotated.html"},
+{text:"Data Structure Index",url:"classes.html"},
+{text:"Data Fields",url:"functions.html",children:[
+{text:"All",url:"functions.html"},
+{text:"Functions",url:"functions_func.html"}]}]},
+{text:"Files",url:"files.html",children:[
+{text:"File List",url:"files.html"},
+{text:"Globals",url:"globals.html",children:[
+{text:"All",url:"globals.html"},
+{text:"Enumerations",url:"globals_enum.html"},
+{text:"Enumerator",url:"globals_eval.html"}]}]},
+{text:"Examples",url:"examples.html"}]}
